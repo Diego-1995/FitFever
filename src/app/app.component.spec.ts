1b87331ce600e8000657f739ca539302719cf13b
diego95 <diego.cremesini7@gmail.com>
2024-04-21 17:34:37 +0200

    feat(configuration): add angular material

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,6 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('fitFever');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fitFever');
-  });
+  // Check if has router outlet
+
 });
